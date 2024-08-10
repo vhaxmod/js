800314767b88573350b254d74c9a24870266fc53
vhaxmod <hvung2007@gmail.com>
2024-08-10 18:27:06 +0700

    Update vhaxmod.js

--- vhaxmod.js
+++ vhaxmod.js
@@ -1,3 +1,425 @@
+function bsp() {
+    h5gg.clearResults();
+    h5gg.searchNumber('4397530849764387586', 'I64', '0x100000000', '0x200000000');
+    alert('𝐂𝐡𝐨̣𝐧 𝐒𝐩𝐞𝐞𝐝(' + h5gg.getResultsCount() + '/1)');
+}
+
+function spx10() {
+    h5gg.editAll('4397530849749489418', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: '𝐒𝐩𝐞𝐞𝐝 𝐱𝟏𝟎 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+}
+
+function tsp() {
+    h5gg.editAll('4397530849764387586', 'I64');
+    vant.showNotify({
+        type: 'success',
+        message: '𝐒𝐩𝐞𝐞𝐝 Đ𝐚̃ 𝐓𝐚̆́𝐭'
+    });
+
+}
+
+
+function spx50() {
+    h5gg.editAll('4397530849741637681', 'I64');
+    vant.showNotify({
+        type: 'success',
+        message: '𝐒𝐩𝐞𝐞𝐝 𝐱𝟓𝟎 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+}
+
+function spx2() {
+    h5gg.editAll('4397530849758414897', 'I64');
+    vant.showNotify({
+        type: 'success',
+        message: '𝐒𝐩𝐞𝐞𝐝 𝐱𝟐 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+}
+
+
+
+
+
+
+
+
+function dsn() {
+    h5gg.clearResults();
+    h5gg.searchNumber('72340173878198272', 'I64', '0x100000000', '0x130000000');
+    h5gg.editAll('72340172821233664', 'I64');
+    vant.showNotify({
+        type: 'success',
+        message: 'Đ𝐨̂̉𝐢 𝐒𝐮́𝐧𝐠 𝐍𝐡𝐚𝐧𝐡 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+
+}
+
+function s04() {
+    h5gg.clearResults();
+    h5gg.searchNumber('4609164001829663539', 'I64', '0x100000000', '0x130000000');
+    h5gg.editAll('4620693218765517619', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Success'
+    });
+
+
+}
+
+function s05() {
+    h5gg.clearResults();
+    h5gg.searchNumber('1065856532', 'I64', '0x100000000', '0x160000000');
+    h5gg.editAll('1073741824', 'I64');
+
+    vant.showNotify({
+        type: 'success',
+        message: 'Success'
+    });
+
+
+}
+
+function antenvai() {
+    h5gg.clearResults();
+    h5gg.searchNumber('4575657222469899220', 'I64', '0x100000000', '0x160000000');
+    h5gg.editAll('4848124999925814228', 'I64');
+    h5gg.clearResults();
+    h5gg.searchNumber('4575657222469336965', 'I64', '0x100000000', '0x160000000');
+    h5gg.editAll('4848124999925251973', 'I64');
+    vant.showNotify({
+        type: 'success',
+        message: '𝐀𝐍𝐓𝐄𝐍 𝐕𝐚𝐢 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+
+}
+
+
+
+function antendau() {
+    h5gg.clearResults();
+    h5gg.searchNumber('1051196712', 'I64', '0x100000000', '0x160000000');
+    var count = h5gg.getResultsCount();
+    var r = h5gg.getResults(count);
+    var cnt = 0;
+    for (var i = 0; i < count; i++) {
+        var addr1 = r[i].address;
+
+        var dizhi0 = (Number(addr1) + 20);
+
+
+
+
+        {
+            h5gg.setValue(dizhi0, 200, "F32");
+        }
+    }
+    h5gg.clearResults();
+    h5gg.searchNumber('4575657224610966592', 'I64', '0x100000000', '0x160000000');
+    h5gg.editAll('4848125002066881600', 'I64');
+    vant.showNotify({
+        type: 'success',
+        message: '𝐀𝐍𝐓𝐄𝐍 Đ𝐚̂̀𝐮 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+
+}
+
+function antentay() {
+    h5gg.clearResults();
+    h5gg.searchNumber('4575657222443971598', 'I64', '0x100000000', '0x130000000');
+    h5gg.editAll('4848124999899886606', 'I64');
+    h5gg.clearResults();
+    h5gg.searchNumber('4575657224578539469', 'I64', '0x100000000', '0x130000000');
+    h5gg.editAll('4848125002034454477', 'I64');
+    vant.showNotify({
+        type: 'success',
+        message: '𝐀𝐍𝐓𝐄𝐍 𝐓𝐚𝐲 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+
+}
+
+function bman() {
+    h5gg.clearResults();
+    var hk1 = prompt("請輸入想要的靈敏");
+    h5gg.searchNumber('4417130516484980736', 'I64', '0x100000000', '0x160000000');
+    var count = h5gg.getResultsCount();
+    var r = h5gg.getResults(count);
+    var cnt = 0;
+    for (var i = 0; i < count; i++) {
+        var addr1 = r[i].address;
+        var dizhi0 = (Number(addr1) + -4);
+        var dizhi1 = (Number(addr1) + -8);
+        var dizhi2 = (Number(addr1) + -12);
+        var dizhi3 = (Number(addr1) + -0);
+
+
+
+
+        {
+            h5gg.setValue(dizhi0, hk1, "F32");
+            h5gg.setValue(dizhi1, hk1, "F32");
+            h5gg.setValue(dizhi2, hk1, "F32");
+            h5gg.setValue(dizhi3, hk1, "F32");
+
+        }
+    }
+    vant.showNotify({
+        type: 'success',
+        message: '𝐁𝐮𝐟𝐟 𝐌𝐚̀𝐧 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+}
+
+function tawm() {
+    h5gg.clearResults();
+    h5gg.searchNumber('4603687625386098688', 'I64', '0x100000000', '0x160000000');
+    var count = h5gg.getResultsCount();
+    var r = h5gg.getResults(count);
+    var cnt = 0;
+    for (var i = 0; i < count; i++) {
+        var addr1 = r[i].address;
+
+        var dizhi0 = (Number(addr1) + 36);
+        var dizhi1 = (Number(addr1) + 40);
+        var dizhi2 = (Number(addr1) + -308);
+
+
+
+        {
+            h5gg.setValue(dizhi0, 0.01, "F32");
+            h5gg.setValue(dizhi1, 0.01, "F32");
+            h5gg.setValue(dizhi2, 0, "I32");
+
+        }
+    }
+    vant.showNotify({
+        type: 'success',
+        message: '𝐓𝐚̂𝐦 𝐀𝐖𝐌 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+
+}
+
+function dsawm() {
+    h5gg.clearResults();
+    h5gg.searchNumber('4603687625386098688', 'I64', '0x100000000', '0x160000000');
+    var count = h5gg.getResultsCount();
+    var r = h5gg.getResults(count);
+    var cnt = 0;
+    for (var i = 0; i < count; i++) {
+        var addr1 = r[i].address;
+
+        var dizhi0 = (Number(addr1) + 36);
+        var dizhi1 = (Number(addr1) + 40);
+
+
+
+        {
+            h5gg.setValue(dizhi0, 0.01, "F32");
+            h5gg.setValue(dizhi1, 0.01, "F32");
+
+        }
+    }
+    vant.showNotify({
+        type: 'success',
+        message: 'Đ𝐨̂̉𝐢 𝐒𝐮́𝐧𝐠 𝐀𝐖𝐌 𝐍𝐡𝐚𝐧𝐡 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+
+}
+
+
+
+
+function nhetam() {
+    h5gg.clearResults();
+    h5gg.searchNumber('4728779608739021057', 'I64', '0x100000000', '0x150000000');
+
+    alert('Sear(' + h5gg.getResultsCount() + ')');
+
+    var count = h5gg.getResultsCount(); //获取总共搜索结果个数
+
+    var r = h5gg.getResults(count); //获取总共搜索结果个数的数组
+
+    //开始循环遍历
+
+    for (var i = 0; i < count; i++) {
+
+        var addr1 = r[i].address; //获取每个搜索结果的地址 address弟子 value值 type类型
+
+        var weishu = /18$/; //设置置顶结尾
+
+        var pd = weishu.test(addr1); //判断地址是否包含指定结尾
+
+
+
+        var dizhi1 = (Number(addr1) + 4);
+
+
+
+
+        if (pd) {
+            h5gg.setValue(dizhi1, 100, "F32");
+
+
+        }
+    }
+    vant.showNotify({
+        type: 'success',
+        message: '𝐍𝐡𝐞̣ 𝐓𝐚̂𝐦 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+
+}
+
+function magic() {
+    h5gg.clearResults();
+    h5gg.searchNumber('4333543704410193920', 'I64', '0x100000000', '0x160000000');
+    h5gg.searchNearby('0.01', 'F32', '0x8');
+    h5gg.searchNearby('0.0219~0.02975', 'F32', '0x32')
+    h5gg.searchNearby('0.1035~0.1070', 'F32', '0x4');
+    h5gg.searchNearby('2.802597e-45', 'F32', '0x4');
+    h5gg.searchNumber('0.1035~0.1070', 'F32', '0x100000000', '0x160000000');
+    h5gg.editAll('1.875', "F32");
+    vant.showNotify({
+        type: 'success',
+        message: '𝐌𝐚𝐠𝐢𝐜 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+}
+
+function s56() {
+    h5gg.clearResults();
+    h5gg.searchNumber('3.741467e-43', 'F32', '0x100000000', '0x170000000');
+    h5gg.searchNearby('0.07~0.0899999', 'F32', '0x20');
+    h5gg.searchNumber('0.07~0.0899999', 'F32', '0x100000000', '0x170000000');
+    h5gg.searchNearby('0.0068~0.00915', 'F32', '0x20');
+    h5gg.searchNearby('0.01', 'F32', '0x45');
+    h5gg.searchNumber('0.07~0.0899999', 'F32', '0x100000000', '0x170000000');
+    h5gg.searchNearby('2', 'I32', '0x4');
+    h5gg.searchNumber('0.07~0.0899999', 'F32', '0x100000000', '0x170000000');
+    h5gg.editAll('0.135', "F32");
+    vant.showNotify({
+        type: 'success',
+        message: '人物鎖頭'
+    });
+}
+function aimawm() {
+h5gg.clearResults();
+h5gg.searchNumber('31101603021', 'I64', '0x100000000', '0x160000000');
+var count = h5gg.getResultsCount();
+var r = h5gg.getResults(count);
+var cnt = 0;
+for(var i=0; i<count; i++) {
+    var addr1 = r[i].address;
+
+var dizhi0 = (Number(addr1) + 8);
+
+   
+
+
+{
+h5gg.setValue(dizhi0, 2139095040, "I32");
+
+   }
+}
+vant.showNotify({
+        type: 'success',
+        message: '𝐀𝐈𝐌 𝐀𝐖𝐌 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+}
+
+
+
+
+function s2296() {
+    h5gg.clearResults();
+    h5gg.searchNumber('8128164201335532067', 'I64', '0x100000000', '0x160000000');
+    var count = h5gg.getResultsCount();
+    var r = h5gg.getResults(count);
+    var cnt = 0;
+    for (var i = 0; i < count; i++) {
+        var addr1 = r[i].address;
+
+        var dizhi0 = (Number(addr1) + 4);
+        var dizhi1 = (Number(addr1) + -12);
+        var dizhi2 = (Number(addr1) + -16);
+        var dizhi3 = (Number(addr1) + -20);
+        var dizhi4 = (Number(addr1) + -48);
+
+
+
+
+        {
+            h5gg.setValue(dizhi0, 2753558450, "I32");
+            h5gg.setValue(dizhi1, 13, "F32");
+            h5gg.setValue(dizhi2, 13, "F32");
+            h5gg.setValue(dizhi3, 13, "F32");
+            h5gg.setValue(dizhi4, 0.7, "F32");
+
+        }
+    }
+
+
+    h5gg.clearResults();
+    h5gg.searchNumber('506812498028', 'I64', '0x100000000', '0x160000000');
+    var count = h5gg.getResultsCount();
+    var r = h5gg.getResults(count);
+    var cnt = 0;
+    for (var i = 0; i < count; i++) {
+        var addr1 = r[i].address;
+
+
+        var dizhi1 = (Number(addr1) + 180);
+        var dizhi2 = (Number(addr1) + 184);
+        var dizhi3 = (Number(addr1) + 188);
+        var dizhi4 = (Number(addr1) + 192);
+        var dizhi5 = (Number(addr1) + 196);
+        var dizhi6 = (Number(addr1) + 200);
+
+
+
+
+        {
+            h5gg.setValue(dizhi1, 5439583, "I32");
+            h5gg.setValue(dizhi2, 6881392, "I32");
+            h5gg.setValue(dizhi3, 6619246, "I32");
+            h5gg.setValue(dizhi4, 0, "I32");
+            h5gg.setValue(dizhi5, 0, "I32");
+            h5gg.setValue(dizhi6, 0, "I32");
+
+        }
+    }
+
+    vant.showNotify({
+        type: 'success',
+        message: '範圍'
+    });
+
+}
+function tp() {
+        h5gg.clearResults();
+        h5gg.searchNumber('914000001','I32','0x100000000', '0x200000000');
+        h5gg.editAll('914000002', 'I32'); 
+        h5gg.clearResults();
+
+		vant.showNotify({
+        type: 'success',
+        message: 'Mod Trang Phục Đã Bật'
+    });
+
+    }
+
+
 function aimcopev1() { 
         function searchAndReplace(searchValue, 
         replaceValue, 
@@ -7,15 +429,29 @@ function aimcopev1() {
         '0x100000000', 
         '0x160000000'); var results = h5gg.getResults(h5gg.getResultsCount()); for (var i = 0; i < results.length; i++) { 
         h5gg.setValue(results[i].address, replaceValue.toString(), type)}} 
-        searchAndReplace(0.09, 180, 'F32')} 
+        searchAndReplace(0.09, 180, 'F32')
+
+vant.showNotify({
+        type: 'success',
+        message: '𝐀𝐈𝐌 𝐂𝐎𝐏𝐄 𝐯𝟏 Đ𝐚̃ 𝐁𝐚̣̂𝐭'
+    });
+
+    }
  
  
  
-   function norecoi() { 
+   function norecoil() { 
         function searchAndReplace(searchValue, replaceValue, type) { 
         h5gg.clearResults(); h5gg.searchNumber(searchValue, type, '0x100000000', '0x160000000'); var results = h5gg.getResults(h5gg.getResultsCount()); for (var i = 0; i < results.length; i++) { 
         h5gg.setValue(results[i].address, replaceValue.toString(), type)}} 
-        searchAndReplace(1016018816, 180, 'I32')} 
+        searchAndReplace(1016018816, 180, 'I32')
+
+vant.showNotify({
+        type: 'success',
+        message: 'Đ𝐚̣𝐧 𝐓𝐡𝐚̆̉𝐧𝐠 Đã Bật'
+    });
+
+    }
  
  
    
@@ -27,10 +463,15 @@ function aimcopev1() {
   searchAndReplace(1057048494, -2000, 'I32') 
         searchAndReplace(1054951342, -2000, 'I32') 
  
-  } 
+  vant.showNotify({
+        type: 'success',
+        message: '𝐀𝐈𝐌 𝟏𝟖𝟎 Đã Bật'
+    });
+
+    }
  
  
-        function nhetam() { // xóa hoạt ảnh // 
+        function xoahoatanh() { // xóa hoạt ảnh // 
         function searchAndReplace(searchValue, replaceValue, type) { 
         h5gg.clearResults(); 
         h5gg.searchNumber(searchValue, type, '0x100000000', '0x160000000'); 
@@ -40,14 +481,28 @@ function aimcopev1() {
         } 
     } 
     searchAndReplace(3145840, 49, 'I32'); 
-} 
+
+	vant.showNotify({
+        type: 'success',
+        message: '𝐗𝐨𝐚́ 𝐇𝐨𝐚̣𝐭 𝐀̉𝐧𝐡 Đã Bật'
+    });
+
+    }
  
  
   function hscu() { 
         function searchAndReplace(searchValue, replaceValue, type) { 
         h5gg.clearResults(); h5gg.searchNumber(searchValue, type, '0x100000000', '0x200000000'); var results = h5gg.getResults(h5gg.getResultsCount()); for (var i = 0; i < results.length; i++) { 
         h5gg.setValue(results[i].address, replaceValue.toString(), type)}} 
-        searchAndReplace(2018908708, 96688289, 'I32')} 
+        searchAndReplace(2018908708, 96688289, 'I32')
+
+vant.showNotify({
+        type: 'success',
+        message: '𝐇𝐒 𝐂𝐮 Đã Bật'
+    });
+
+    }
+
 
 
         function aimcopev2() { 
@@ -59,7 +514,14 @@ function aimcopev1() {
         '0x100000000', 
         '0x160000000'); var results = h5gg.getResults(h5gg.getResultsCount()); for (var i = 0; i < results.length; i++) { 
         h5gg.setValue(results[i].address, replaceValue.toString(), type)}} 
-        searchAndReplace(1075000115, -5, 'F32')} 
+        searchAndReplace(1075000115, -5, 'F32')
+
+vant.showNotify({
+        type: 'success',
+        message: '𝐀𝐈𝐌 𝐂𝐎𝐏𝐄 𝐯𝟐 Đã Bật'
+    });
+
+    }
          
 
 
@@ -101,10 +563,26 @@ if (searchValue === Nhayman) Nhayman = replaceValue;
 function buffnhay() { 
     modifyValue1(nhayngam, Number(prompt("Nhập Độ Nhạy Màn Hình :", "")), 'BB4', null, 'Đã Thay Đổi Độ Nhạy Màn Hình'); 
     modifyValue1(Nhayman, Number(prompt("Nhập Độ Nhạy Kéo Tâm :", "")), 'BAC', null, 'Đã Thay Đổi Độ Nhạy Kéo Tâm'); 
+
+vant.showNotify({
+        type: 'success',
+        message: '𝐁𝐮𝐟𝐟 𝐍𝐡𝐚̣𝐲 Đã Bật'
+    });
+
+    
 } 
 
 
 function aimlock() { 
     modifyValue(1077936128, -20000, 'A04');  
-    modifyValue(1069547520, 1148829696, 'A10')}
+    modifyValue(1069547520, 1148829696, 'A10')
 
+vant.showNotify({
+        type: 'success',
+        message: '𝐀𝐈𝐌 𝐋𝐎𝐂𝐊 Đã Bật'
+    });
+
+    }
+ 
+ 
+        
